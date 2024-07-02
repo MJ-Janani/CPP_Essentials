606cb20257cd4b9df8d225139c4ea7745c3b90d4
Janani M <141489026+MJ-Janani@users.noreply.github.com>
2024-07-02 17:12:00 +0530

    Create functions_intro.cpp

---
+++ Functions/functions_intro.cpp
@@ -0,0 +1,8 @@
+//Function = Declaaration+ Definition
+void play_music(){
+  cout<<"Enter your favorite song: "<<endl;
+  int song;
+  cin>>song;
+  cout<<"Playing song "<<song;
+}//once defined
+play_music();//calling the function

ed7f741a4830be15dfb2124062334ffa567276cf
Janani M <141489026+MJ-Janani@users.noreply.github.com>
2024-07-03 10:12:57 +0530

    Update parameters.cpp

--- Functions/parameters.cpp
+++ Functions/parameters.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void play_music(int songId, float duration){//variables defined inside function=parameters
+void play_music(int songId, float duration=10){//variables defined inside function=parameters
   cout<<"Playing song number "<< songId <<" for "<<duration<< " minutes"<<endl;
 } 
 int main(){
-    play_music(1,5.5);
+    play_music(1,5.5);//arguments
     play_music(2,3);
-    play_music(3,4.2);
+    play_music(3);//default value is assigned to the duration i.e.10
     return 0;
 }
 /*OUTPUT:
-Playing song number 1 for 5.5minutes
-Playing song number 2 for 3minutes
-Playing song number 3 for 4.2minutes
+Playing song number 1 for 5.5 minutes
+Playing song number 2 for 3 minutes
+Playing song number 3 for 10 minutes
 
-
-=== Code Execution Successful ===
 */
